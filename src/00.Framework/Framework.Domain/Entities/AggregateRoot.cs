e3f25eafee5bc0b87ee1b59d6096475839b1dda9
HasanRezaei <HasanRezaei3790@gmail.com>
2024-05-18 15:46:54 +0330

    base class added

---
+++ src/00.Framework/Framework.Domain/Entities/AggregateRoot.cs
@@ -0,0 +1,15 @@
+﻿using Framework.Domain.Events;
+
+namespace Framework.Domain.Entities;
+
+public class AggregateRoot
+{
+    private readonly Queue<DomainEvent> _events = new();
+    public IEnumerable<DomainEvent> Events => _events;
+
+    public void ClearEvents()
+        => _events.Clear();
+
+    protected void AppendEvent(DomainEvent e)
+        => _events.Enqueue(e);
+}
