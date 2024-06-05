d783b46d8658001112dff0d998a797b748d4651e
Hasan Rezaei <hasanrezaei3790@gmail.com>
2024-06-05 18:28:09 +0330

    chore: add test tools layer

---
+++ test/BaseArchitecture.Application.Tests/Employees/RegisterEmployeeHandlerTests.cs
@@ -0,0 +1,12 @@
+﻿using BaseArchitecture.ApplicationServices.Employees;
+using BaseArchitecture.Domain.Employees.Data;
+using BaseArchitecture.Persistence.EF.Employees;
+using BaseArchitecture.TestTools;
+
+namespace BaseArchitecture.Application.Tests.Employees
+{
+    public class RegisterEmployeeHandlerTests
+    {
+        private readonly RegisterEmployeeHandler _sut;
+    }
+}
